9898d74e81c3daa455ed844a491220ec197cb4f7
danielnoo <dannynoo@gmail.com>
2021-11-04 17:30:23 -0400

    added and configured Morgan for logging

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const PORT = 3001
-const { response } = require('express');
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express();
 
-app.use(express.json());
+
+const requestLogger = (req, res, next) => {
+  console.log("Method:", req.method);
+  console.log("Path:  ", req.path);
+  console.log("Body:  ", req.body);
+  console.log("---");
+  next();
+};
+
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(express.json())
+app.use(requestLogger)
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+)
+
 
 let people = [
   {
@@ -82,10 +110,34 @@ app.delete('/api/persons/:id', (req, res) => {
 app.post('/api/persons/', (req, res) => {
   // create random id for new entry
   const randomID = Math.floor(Math.random() * 999999999999)
-  console.log(req.body)
-  res.json('hello')
-})
+  
+  // if request body doesnt not include name or number, send back 400
+  // if name is already used in database send back 400
 
+  if(!req.body.name || !req.body.number) {
+    return res.status(400).json({ error: "content missing", })
+  } else if(people.filter(person => person.name === req.body.name).length > 0) {
+    return res.status(400).json({ error: "name already in use",})
+  }
+
+  const newEntry = {
+    "id": randomID,
+    "name": req.body.name,
+    "number": req.body.number
+  }
+
+  people = people.concat(newEntry)
+  
+  res.json(newEntry)
+
+})
+  
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 
 // listen on PORT
