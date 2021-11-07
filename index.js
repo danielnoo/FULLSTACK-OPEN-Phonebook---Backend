9993f3019170e1c816b5d993c3f384222a7f57f3
danielnoo <dannynoo@gmail.com>
2021-11-07 15:10:55 -0500

    adding mongoose and dotenv functionality

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
-
-
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
 
 const requestLogger = (req, res, next) => {
   console.log("Method:", req.method);
@@ -18,11 +16,9 @@ morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-
-
 app.use(express.json());
-app.use(requestLogger)
-app.use(express.static("build"))
+app.use(requestLogger);
+app.use(express.static("build"));
 app.use(cors());
 app.use(
   morgan(function (tokens, req, res) {
@@ -37,8 +33,7 @@ app.use(
       tokens.body(req, res),
     ].join(" ");
   })
-)
-
+);
 
 let people = [
   {
@@ -68,92 +63,88 @@ let people = [
 ];
 
 /// GET request from base api url
-app.get('/', (req, res) => {
-  res.send(`<h1>HELLO</h1>`)
-})
+app.get("/", (req, res) => {
+  res.send(`<h1>HELLO</h1>`);
+});
 
 // GET base API route
 
-app.get('/api/persons', (req, res) => {
-  res.json(people)
-})
-
+app.get("/api/persons", (req, res) => {
+  res.json(people);
+});
 
 // GET phonebook info
 
-app.get('/info', (req, res) => {
-  
-  const currentTime = new Date()
+app.get("/info", (req, res) => {
+  const currentTime = new Date();
   const response = `<p>Phonebook has info for ${people.length} people 
   
   </br>
   </br> ${currentTime}</p>`;
-  res.send(response)
-})
+  res.send(response);
+});
 
 // GET specific entry
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  
-  const response = people.find(person => person.id === id)
-    
-  if(response) {
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const response = people.find((person) => person.id === id);
+
+  if (response) {
     res.json(response);
   } else {
-    res.status(404).end()
+    res.status(404).end();
   }
-  
-})
+});
 
 // delete an item (at this point only from local memory)
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  people = people.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  people = people.filter((person) => person.id !== id);
 
-  res.status(204).end()
-})
+  res.status(204).end();
+});
 
 // POST an item and assign it a random id
 
-app.post('/api/persons/', (req, res) => {
+app.post("/api/persons/", (req, res) => {
   // create random id for new entry
-  const randomID = Math.floor(Math.random() * 999999999999)
-  
+  const randomID = Math.floor(Math.random() * 999999999999);
+
   // if request body doesnt not include name or number, send back 400
   // if name is already used in database send back 400
 
-  if(!req.body.name || !req.body.number) {
-    return res.status(400).json({ error: "content missing", })
-  } else if(people.filter(person => person.name === req.body.name).length > 0) {
-    return res.status(400).json({ error: "name already in use",})
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({ error: "content missing" });
+  } else if (
+    people.filter((person) => person.name === req.body.name).length > 0
+  ) {
+    return res.status(400).json({ error: "name already in use" });
   }
 
   const newEntry = {
-    "id": randomID,
-    "name": req.body.name,
-    "number": req.body.number
-  }
+    id: randomID,
+    name: req.body.name,
+    number: req.body.number,
+  };
 
-  people = people.concat(newEntry)
-  
-  res.json(newEntry)
+  people = people.concat(newEntry);
 
-})
-
+  res.json(newEntry);
+});
 
 /// PUT replace object in persons with updated
 
-app.put('/api/persons/:id', (req, res) => {
-  const newEntry = req.body
-  people = people.map(person => person.id !== newEntry.id ? person : newEntry)
+app.put("/api/persons/:id", (req, res) => {
+  const newEntry = req.body;
+  people = people.map((person) =>
+    person.id !== newEntry.id ? person : newEntry
+  );
 
-  res.json(newEntry)
-
-})
-
-
+  res.json(newEntry);
+});
 
 /// handling for unknown endpoint
 
@@ -163,13 +154,24 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint);
 
-
 // listen on PORT
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+
+const mongoose = require("mongoose");
+
+const url = process.env.MONGO_URI;
+console.log(url);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
 
 app.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
-})
-
-
+});
